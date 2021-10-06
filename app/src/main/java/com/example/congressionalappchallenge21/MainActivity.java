79bdd1fe85afb4766c63244246f92e1382142e60
Cadence <cadence242@roadrunner.com>
2021-10-06 08:37:07 -0700

    added play button

--- app/src/main/java/com/example/congressionalappchallenge21/MainActivity.java
+++ app/src/main/java/com/example/congressionalappchallenge21/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.congressionalappchallenge21;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void playClick(View v)
+    {
+        Toast.makeText(this, "You clicked play", Toast.LENGTH_SHORT).show();
+        Log.i("info", "The user clicked play");
     }
 }
