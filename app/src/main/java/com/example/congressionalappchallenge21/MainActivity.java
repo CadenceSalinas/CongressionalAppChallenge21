c0d830b0ca829c5175fe48dd7eb94ca004817420
Cadence <cadence242@roadrunner.com>
2021-10-12 08:12:13 -0700

    play button switches to next screen

--- app/src/main/java/com/example/congressionalappchallenge21/MainActivity.java
+++ app/src/main/java/com/example/congressionalappchallenge21/MainActivity.java
@@ -2,17 +2,38 @@ package com.example.congressionalappchallenge21;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button playButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        playButton = (Button)findViewById(R.id.play);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v)
+            {
+
+                // Intents are objects of the android.content.Intent type. Your code can send them
+                // to the Android system defining the components you are targeting.
+                // Intent to start an activity called ThirdActivity with the following code:
+
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+
+                // start the activity connect to the specified class
+                startActivity(intent);
+            }
+        });
     }//end onCreate
 
     public void playClick(View v)
